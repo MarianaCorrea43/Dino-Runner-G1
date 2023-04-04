0b94ce157cc4b49df6662c9bde71f85a865ae0a3
Marianacorrea43 <mariana.apc02@gmail.com>
2023-04-04 17:21:48 -0500

    update

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -10,7 +10,8 @@ DINO_DUCKING = "DUCKING"
 
 class Dinosaur(Sprite):
     POS_X = 80
-    POS_Y = 310
+    POS_Y = 310 
+    POS_Y_DUCK = POS_Y +35 
     JUMP_VELOCITY = 8.5
 
     def __init__(self):
@@ -45,13 +46,21 @@ class Dinosaur(Sprite):
                 self.step = 0
 
     def duck(self):
-        self.image = DUCKING[self.step // 5]
+        self.update_image(DUCKING[self.step //5], pos_y=self.POS_Y_DUCK)
+        self.step += 1
+
+    def update_image(self, image: pygame.Surface, pos_x=None, pos_y=None):
+        self.image = image
+        self.rect = image.get_rect()
+        self.rect.x = pos_x or self.POS_X
+        self.rect.y = pos_y or self.POS_Y 
+
+        """self.image = DUCKING[self.step // 5]
         self.rect = self.image.get_rect()
         self.rect.x = self.POS_X
         self.rect.y = self.POS_Y + 35
         self.step += 1
-        self.position()
-
+        self.position()"""
 
     def run(self):
         self.image = RUNNING[self.step // 5]
@@ -60,7 +69,6 @@ class Dinosaur(Sprite):
         self.rect.y = self.POS_Y
         self.step += 1
         self.position()
-
 
     def jump(self):
         self.image = JUMPING
